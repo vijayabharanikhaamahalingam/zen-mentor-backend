0992cd7f08d65adccbae7e46c4ed4cbf2b878bae
viji <vijayabharanikhaamahalingam@gmail.com>
2024-08-25 23:32:51 +0530

    first commit

--- index.js
+++ index.js
@@ -17,27 +17,31 @@ const URL = process.env.MONGODB_URL;
 
 const mongoose = require('mongoose');
 
-mongoose.connect(URL,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
-const connection = mongoose.connection;
-connection.on('open',() => console.log("MongoDB Connected"));
+mongoose.connect(URL)
+  .then(
+    () => {
+      console.log('Connected to MongoDB');
+     
+      app.listen(PORT, () => console.log(`Server started in the port ${PORT}`))
+    });
+
+// app.get('/',(req,res) => res.send(`
+// // <div>
+// // <p> In Home Page </p>
+// // <p>To get all mentor List - https://zen-assign-mentors.herokuapp.com/Mentors </p>
+// // <br>
+// // <p>To get all Students List - https://zen-assign-mentors.herokuapp.com/Students </p>
+// // <br>
+// // <p>To get mentor based on ID - https://zen-assign-mentors.herokuapp.com/Mentors/get-mentor/:id<p>
+// // <p>sample - https://zen-assign-mentors.herokuapp.com/Mentors/get-mentor/60e7f515d5ff5342a06652e3 </p>
+
+// // <p> To test Post and update - visit Frontend page of the application - https://preethi-st.github.io/ZEN-Mentors-Frontend/ </p>
+// // </div>
 
 
-
-app.get('/',(req,res) => res.send(`
-<div>
-<p> In Home Page </p>
-<p>To get all mentor List - https://zen-assign-mentors.herokuapp.com/Mentors </p>
-<br>
-<p>To get all Students List - https://zen-assign-mentors.herokuapp.com/Students </p>
-<br>
-<p>To get mentor based on ID - https://zen-assign-mentors.herokuapp.com/Mentors/get-mentor/:id<p>
-<p>sample - https://zen-assign-mentors.herokuapp.com/Mentors/get-mentor/60e7f515d5ff5342a06652e3 </p>
-
-<p> To test Post and update - visit Frontend page of the application - https://preethi-st.github.io/ZEN-Mentors-Frontend/ </p>
-</div>
-`))
+// `))
 
 app.use('/Mentors',mentorRouter);
 app.use('/Students',studentRouter);
 
-app.listen(PORT, () => console.log(`Server started in the port ${PORT}`))
+
